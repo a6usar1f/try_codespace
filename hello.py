7af08dce9bbf95746787db291489611b79032ab8
Agus Arif <a6usar1f@yahoo.com>
2023-04-04 09:57:43 +0000

    Make the first program in Python

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello.py: My first program in Python on GitHub Codespace, yay!
+# Agus Arif, 04-04-2023
+
+# display a classic message
+print('Hello, World!')
+
+# display a specific message
+print()
+name = input()
